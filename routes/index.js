282990ec0d86eae83ca549517360e8d8e9a93340
AJLV19 <theajlv19@gmail.com>
2019-01-28 16:16:27 -0400

    arturo

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/aviones', function(req, res, next){
+  res.render('aviones');
+});
+
+router.get('/caracteristicas_Del_Avion', function(req, res, next){
+  res.render('caracteristicas_Del_Avion');
+});
+
+router.get('/', function(req, res, next){
+  res.render('index');
+});
+
 module.exports = router;
